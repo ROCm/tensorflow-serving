3df2074167c518f9f84f5d3fc0913564b476fb6e
Wang, Yanyao <yanyao.wang@amd.com>
2023-01-24 10:09:02 -0800

    Update install_tf_rocm_whl.bash

--- tensorflow_serving/tools/docker/install_tf_rocm_whl.bash
+++ tensorflow_serving/tools/docker/install_tf_rocm_whl.bash
@@ -10,4 +10,4 @@ TF_VERS_SUBSCRIPT=$( \
     ) && \
     python3 -m pip uninstall -y tensorflow-gpu tensorflow-estimator keras keras-nightly && \
     python3 -m pip install tensorflow-rocm==${TF_VERS_TO_POST_INSTALL}.${TF_VERS_SUBSCRIPT} -f ${TF_REPO} || \
-        echo "tensorflow-rocm is not installed correctly" && exit 2
+        (echo "tensorflow-rocm is not installed correctly" && exit 2)
