946ab3c2018bf0bf072cddf5f0ab51bafb78c2ab
Jason Furmanek <Jason.Furmanek@amd.com>
2024-04-30 14:23:14 -0700

    Update to 2.15 and ROCM 6.1

--- tensorflow_serving/tools/docker/install_tf_rocm_whl.bash
+++ tensorflow_serving/tools/docker/install_tf_rocm_whl.bash
@@ -1,13 +1,15 @@
 #!/bin/bash
 
-TF_REPO="https://pypi.org"
-TF_VERS_TO_POST_INSTALL="2.14.0"
+#TF_REPO="https://pypi.org"
+TF_REPO="https://repo.radeon.com/rocm/manylinux/rocm-rel-6.1/"
+TF_VERS_TO_POST_INSTALL="2.15.0"
 TF_VERS_SUBSCRIPT=$( \
                     basename $ROCM_PATH | \
                     cut -d "-" -f2 | \
                     tr -d "." || \
                     true \
     ) && \
-    python3 -m pip uninstall -y tensorflow-gpu tensorflow-estimator keras keras-nightly && \
-    python3 -m pip install tensorflow-rocm==${TF_VERS_TO_POST_INSTALL}.${TF_VERS_SUBSCRIPT} -f ${TF_REPO} || \
+    python3 -m pip uninstall -y tensorflow tensorflow-gpu tensorflow-estimator keras keras-nightly && \
+    #python3 -m pip install tensorflow-rocm==${TF_VERS_TO_POST_INSTALL}.${TF_VERS_SUBSCRIPT} -f ${TF_REPO} || \
+    python3 -m pip install tensorflow-rocm==${TF_VERS_TO_POST_INSTALL} -f ${TF_REPO} || \
         (echo "tensorflow-rocm is not installed correctly" && exit 2)
