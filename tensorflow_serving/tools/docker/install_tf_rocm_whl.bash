ef9386f615d4e999f3d9249a0523e4a103376514
Jason Furmanek <Jason.Furmanek@amd.com>
2024-04-30 21:43:56 -0700

    Update to 2.16

--- tensorflow_serving/tools/docker/install_tf_rocm_whl.bash
+++ tensorflow_serving/tools/docker/install_tf_rocm_whl.bash
@@ -2,7 +2,7 @@
 
 #TF_REPO="https://pypi.org"
 TF_REPO="https://repo.radeon.com/rocm/manylinux/rocm-rel-6.1/"
-TF_VERS_TO_POST_INSTALL="2.15.0"
+TF_VERS_TO_POST_INSTALL="2.16.0"
 TF_VERS_SUBSCRIPT=$( \
                     basename $ROCM_PATH | \
                     cut -d "-" -f2 | \
