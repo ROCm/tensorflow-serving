fca34e284570d858856c537fce25118157285b8f
kithumma <kithumma@amd.com>
2024-11-14 18:50:26 +0000

    add ENV ROCMVERSION

--- tensorflow_serving/tools/docker/setup.rocm.sh
+++ tensorflow_serving/tools/docker/setup.rocm.sh
@@ -167,5 +167,7 @@ echo $ROCM_PATH
 echo $GPU_DEVICE_TARGETS
 
 # Ensure the ROCm target list is set up
+mkdir -p ${ROCM_PATH}/bin
+mkdir -p ${ROCM_PATH}/.info  
 printf '%s\n' ${GPU_DEVICE_TARGETS} | tee -a "$ROCM_PATH/bin/target.lst"
 touch "${ROCM_PATH}/.info/version"
