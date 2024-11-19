f1545e48913216eab8c5feb8366aefa888f432f3
kithumma <kithumma@amd.com>
2024-11-19 20:33:55 +0000

    code cleanup PR

--- tensorflow_serving/tools/docker/setup.rocm.sh
+++ tensorflow_serving/tools/docker/setup.rocm.sh
@@ -80,35 +80,10 @@ else
 fi
 
 if [[ "$DISTRO" == "focal" ]] || [[ "$DISTRO" == "jammy" ]] || [[ "$DISTRO" == "noble" ]]; then
-    #ROCM_DEB_REPO_HOME=https://repo.radeon.com/rocm/apt/
-
-    #ROCM_BUILD_NAME=${DISTRO}
-    #ROCM_BUILD_NUM=main
-    #AMDGPU_DEB_REPO_HOME=https://repo.radeon.com/amdgpu/
-
-    # Adjust the ROCM repo location
-    #ROCM_DEB_REPO=${ROCM_DEB_REPO_HOME}${ROCM_VERS}/
-
-    #AMDGPU_DEB_REPO=${AMDGPU_DEB_REPO_HOME}${ROCM_VERS}/
 
     DEBIAN_FRONTEND=noninteractive apt-get --allow-unauthenticated update 
     DEBIAN_FRONTEND=noninteractive apt install -y wget software-properties-common
     DEBIAN_FRONTEND=noninteractive apt-get clean all
-
-        # Make the directory if it doesn't exist yet.
-    # This location is recommended by the distribution maintainers.
-
-    #mkdir --parents --mode=0755 /etc/apt/keyrings
-
-    # Download the key, convert the signing-key to a full
-    # keyring required by apt and store in the keyring directory
-    #wget https://repo.radeon.com/rocm/rocm.gpg.key -O - | \
-    #    gpg --dearmor | tee /etc/apt/keyrings/rocm.gpg > /dev/null
-
-    #echo "deb [arch=amd64 signed-by=/etc/apt/keyrings/rocm.gpg] $AMDGPU_DEB_REPO/ubuntu $ROCM_BUILD_NAME $ROCM_BUILD_NUM" | tee --append /etc/apt/sources.list.d/amdgpu.list
-    
-    #echo "deb [arch=amd64 signed-by=/etc/apt/keyrings/rocm.gpg trusted=yes] $ROCM_URL $ROCM_BUILD_NAME $ROCM_BUILD_NUM" | tee /etc/apt/sources.list.d/rocm.list
-  
 
     if [[ "$ROCM_URL" == *"repo.radeon.com"* ]]; then 
         echo "ROCM_URL contains repo.radeon.com"  
@@ -122,9 +97,7 @@ if [[ "$DISTRO" == "focal" ]] || [[ "$DISTRO" == "jammy" ]] || [[ "$DISTRO" == "
         echo -e 'Package: *\nPin: release o=repo.radeon.com\nPin-Priority: 600' | tee /etc/apt/preferences.d/rocm-pin-600  
     else  
         echo "ROCM_URL does not contain repo.radeon.com"  
-        # Set pinning for ARTIFACTORY
         chmod +x /setup_pining.sh
-        #/setup_pining.sh $ROCM_BUILD_NAME $ROCM_BUILD_NUM
     fi  
 
     apt-get update --allow-insecure-repositories
@@ -147,7 +120,6 @@ if [[ "$DISTRO" == "focal" ]] || [[ "$DISTRO" == "jammy" ]] || [[ "$DISTRO" == "
     DEBIAN_FRONTEND=noninteractive apt-get install -y --allow-unauthenticated hipblaslt-dev || true
 
 elif [[ "$DISTRO" == "el7" ]]; then
-    #RPM_ROCM_REPO=http://repo.radeon.com/rocm/yum/${ROCM_VERS}/main
     echo -e "[ROCm]\nname=ROCm\nbaseurl=$ROCM_URL\nenabled=1\ngpgcheck=0" >>/etc/yum.repos.d/rocm.repo
     echo -e "[amdgpu]\nname=amdgpu\nbaseurl=https://repo.radeon.com/amdgpu/${ROCM_VERS}/rhel/7/main/x86_64/\nenabled=1\ngpgcheck=0" >>/etc/yum.repos.d/amdgpu.repo
     yum clean all
@@ -159,7 +131,6 @@ elif [[ "$DISTRO" == "el7" ]]; then
     yum --enablerepo=extras install -y hipblaslt-devel || true
 
 elif [[ "$DISTRO" == "el8" ]]; then
-    #RPM_ROCM_REPO=http://repo.radeon.com/rocm/rhel8/${ROCM_VERS}/main
     echo -e "[ROCm]\nname=ROCm\nbaseurl=$ROCM_URL\nenabled=1\ngpgcheck=1\ngpgkey=https://repo.radeon.com/rocm/rocm.gpg.key" >>/etc/yum.repos.d/rocm.repo
     echo -e "[amdgpu]\nname=amdgpu\nbaseurl=https://repo.radeon.com/amdgpu/${ROCM_VERS}/rhel/8.8/main/x86_64/\nenabled=1\ngpgcheck=1\ngpgkey=https://repo.radeon.com/rocm/rocm.gpg.key" >>/etc/yum.repos.d/amdgpu.repo
     dnf clean all
